79ac052b3b394edc451e5804b132bae623691b8f
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-05-22 13:26:24 +0330

    Add admin redirect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,8 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
-
+    public const HOME = 'home';
+    public const ADMIN = 'admin.home';
     /**
      * The controller namespace for the application.
      *
